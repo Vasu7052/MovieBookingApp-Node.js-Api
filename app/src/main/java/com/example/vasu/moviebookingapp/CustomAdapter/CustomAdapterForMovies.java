30479128b7be453f014308d33872ff310781eb85
Vasu Gupta <androfly.official@gmail.com>
2017-10-05 15:15:51 +0530

    Movie Booking App

--- app/src/main/java/com/example/vasu/moviebookingapp/CustomAdapter/CustomAdapterForMovies.java
+++ app/src/main/java/com/example/vasu/moviebookingapp/CustomAdapter/CustomAdapterForMovies.java
@@ -1,12 +1,14 @@
 package com.example.vasu.moviebookingapp.CustomAdapter;
 
 import android.content.Context;
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import com.example.vasu.moviebookingapp.Model.Movies;
@@ -36,6 +38,7 @@ public class CustomAdapterForMovies extends ArrayAdapter<Movies> implements View
 
 
     private class ViewHolder {
+        public ImageView image ;
         public TextView name ;
         public TextView yearAndGenre ;
         public RatingBar rating ;
@@ -62,7 +65,13 @@ public class CustomAdapterForMovies extends ArrayAdapter<Movies> implements View
             convertView = vi.inflate(R.layout.list_item_for_movies, null);
 
             holder = new ViewHolder();
+            holder.image = (ImageView) convertView.findViewById(R.id.image);
             holder.name = (TextView) convertView.findViewById(R.id.title);
+            holder.yearAndGenre = (TextView) convertView.findViewById(R.id.yearAndGenre);
+            holder.description = (TextView) convertView.findViewById(R.id.description);
+            holder.director = (TextView) convertView.findViewById(R.id.director);
+            holder.stars = (TextView) convertView.findViewById(R.id.stars);
+            holder.rating = (RatingBar) convertView.findViewById(R.id.ratingBar);
 
             convertView.setTag(holder);
 
@@ -71,6 +80,11 @@ public class CustomAdapterForMovies extends ArrayAdapter<Movies> implements View
         }
 
         holder.name.setText(users.getName());
+        holder.yearAndGenre.setText(users.getYear() + " " + users.getGenre());
+        holder.description.setText(users.getDescription());
+        holder.director.setText(users.getDirector());
+        holder.stars.setText(users.getStars());
+        holder.rating.setRating(users.getImdbRating());
 
         return convertView;
 
